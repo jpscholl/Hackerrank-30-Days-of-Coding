c030b550ea2c31c3f14c23b918319cc0cf4a6a80
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-03 14:13:29 -0400

    Add files via upload

---
+++ 30 Days of Code/C#/Practice/GuessTheNumber/GuessTheNumber/GuessNumberGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class GuessNumberGame
+    {
+        public static int cpuNumGenerator()
+        {
+            Random rand = new Random();
+            int cpuNum = rand.Next(1, 100);
+            return cpuNum;
+        }
+
+        public static void NumGuessGame()
+        {
+            int cpuRandomNum = cpuNumGenerator();
+            bool playerWins = false;
+            int numOfGuesses = 0;
+            do
+            {
+                Console.Write("Enter your guess: ");
+                int playerGuess = Convert.ToInt32(Console.ReadLine());
+
+
+                if (playerGuess == cpuRandomNum)
+                {
+                    numOfGuesses++;
+                    Console.WriteLine("\nCPU random Number: " + cpuRandomNum);
+                    Console.WriteLine("You guessed it in " + numOfGuesses + " tries");
+                    playerWins = true;
+                }
+                else if (playerGuess >= 0 && playerGuess <= 100 && playerGuess > cpuRandomNum)
+                {
+                    numOfGuesses++;
+                    Console.WriteLine("You guessed too high!");
+                }
+                else if (playerGuess >= 0 && playerGuess <= 100 && playerGuess < cpuRandomNum)
+                {
+                    numOfGuesses++;
+                    Console.WriteLine("You guesssed too low!");
+                }
+                else
+                {
+                    numOfGuesses++;
+                    Console.WriteLine("Out of range!");
+                }
+            } while (playerWins != true);
+
+            Console.WriteLine("Exiting game");
+
+        }
+    }
+}
---
+++ 30 Days of Code/C#/Practice/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 8/31/2021
+//  Exercise: Guess The Number Game
+//  Description: Creating a game where the computer randomly picks number between 1-100
+//  and you try to guess it in as few tries as possible.
+*/
+using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Guess The Number game!" +
+                "\nCPU is choosing number at random (1 - 100)");
+
+            GuessNumberGame.NumGuessGame();
+
+        }
+    }
+}
