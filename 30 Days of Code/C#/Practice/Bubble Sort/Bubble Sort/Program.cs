fbe014868d017042345bed805f05a48a3a603b61
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-14 15:46:16 -0400

    Add files via upload

---
+++ 30 Days of Code/C#/Practice/Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 9/14/2021
+//  Project: Day 20: Bubble Sort
+//  Description: 30 Days of coding Hackerrank day 20
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int numberOfSwaps = 0;
+
+        // Write your code here
+        for (int i = 0; i < n; i++)
+        {
+            for (int x = 0; x < n - 1; x++)
+            {
+                if (a[x] > a[x + 1])
+                {
+                    int temp = a[x];
+                    a[x] = a[x + 1];
+                    a[x + 1] = temp;
+                    numberOfSwaps++;
+                }
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+            } 
+            
+            Console.WriteLine("Array is sorted in " + numberOfSwaps + " swaps.");
+            Console.WriteLine("First Element: " + a[0]);
+
+        }    
+    }
+}
