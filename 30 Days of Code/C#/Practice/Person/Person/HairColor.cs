19c5b7975c689232ffa4d79fe1c843ae90d92f67
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-14 14:09:14 -0400

    Add files via upload
    Alisases and Enums and other stuff

---
+++ 30 Days of Code/C#/Practice/Person/Person/HairColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public enum HairColor
+    {
+        Blonde, 
+        Brown, 
+        Black, 
+        Red, 
+        Orange, 
+        Pink, 
+        Blue, 
+        Green, 
+        Purple, 
+        Rainbow, 
+        Other
+    }
+}
---
+++ 30 Days of Code/C#/Practice/Person/Person/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 9/14/2021
+//  Exercise: Pointers Day 20 tutorial video
+//  Description: following along with tutorial video
+*/
+using System;
+
+namespace Person
+{
+    class Program
+    {
+        public class Person
+        {
+            public HairColor hairColor = HairColor.Black;
+        }
+
+        public static void Main(string[] args)
+        {
+            Person peterParker = new Person();
+            Person spiderMan = peterParker;
+
+            Console.WriteLine("Before dying hair: " + peterParker.hairColor);
+
+            peterParker.hairColor = HairColor.Pink;
+
+            Console.WriteLine("After dying hair: ");
+            Console.WriteLine("Hair color of Peter Parker: " + peterParker.hairColor);
+            Console.WriteLine("Hair color of Spiderman: " + spiderMan.hairColor);
+
+
+        }
+    }
+}
