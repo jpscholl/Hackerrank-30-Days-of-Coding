476eafb4179931a8759341fa7d2338b714a1e1f2
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-21 12:48:03 -0400

    Add files via upload
    Wasn't much to this one

---
+++ 30 Days of Code/C#/Practice/User/User/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace User
+{
+    class Program
+    {
+        public class User
+        {
+            //class variables
+            String username;
+            String password;
+            int age;
+            HashSet<int> orderIDs;
+
+            //class constructors
+            public User(String customUserName, String customPassword, int customAge, HashSet<int> orderIDs)
+            {
+                this.username = customUserName;
+                this.password = customPassword;
+                this.age = customAge;
+                this.orderIDs = orderIDs;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            HashSet<int> a = new HashSet<int>();
+            a.Add(1212);
+            User jonathan = new User("Aeon", "nicebuns20", 33, a);
+
+            //that was the whole video...??? Not really much to the day 28 video
+        }
+    }
+}
