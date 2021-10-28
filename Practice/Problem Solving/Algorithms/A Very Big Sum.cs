32d42fd7894135b7ba2f43d37b56b069bf85146e
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-10-28 18:11:45 -0400

    Very simple
    I mean...there's a built in function that calculates the sum of an array/list

---
+++ Practice/Problem Solving/Algorithms/A Very Big Sum.cs
@@ -0,0 +1,47 @@
+/*
+//  Author: Jonathan Scholl
+//  Date: 10/28/2021
+//  Project: A Very Big Sum (Hackerrank)
+//  Description: Complete the aVeryBigSum function in the editor below. 
+//  It must return the sum of all array elements.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        return ar.Sum();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
